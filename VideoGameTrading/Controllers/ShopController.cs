f988ac5474c7301c0b292964c38e9ef31f8a5eba
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-11-30 14:28:23 -0800

    Term Project - Draft - v2

---
+++ VideoGameTrading/Controllers/ShopController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VideoGameTrading.Models;
+
+namespace VideoGameTrading.Controllers {
+    public class ShopController : Controller {
+        public IActionResult Index() => View();
+    }
+}
